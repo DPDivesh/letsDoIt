72f0dfc664541842c6cffbacf3528343c1323013
DPDivesh <diveshpatel39@yahoo.com>
2021-04-10 17:55:59 -0700

    Started working on code to add main content and nav to body

--- src/mainContentBuilder.js
+++ src/mainContentBuilder.js
@@ -5,7 +5,7 @@ function mainContentBuilder(){
   //create container
   //Determine what page we're on if we dont use function call event listeners or make this a constructor and have the event listener call said constructor
   //example *click button* calls specificButton.Today();
-  //newProjectBuilder 
+  //newProjectBuilder
 
 }
 
@@ -24,3 +24,12 @@ function defaultButton(){
   //prebuilt buttons
 
 }
+
+function containerBuilder(){
+  const queryBody = document.querySelector(body);
+  console.log(queryBody);
+  const toMain =document.createElement('div');
+  toMain.className = 'mainContentContainer';
+  queryBody.appendChild(toMain);
+
+  }
