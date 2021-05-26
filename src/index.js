fef659a947ca0dc51bfd4db680f2299a2fc664cd
DPDivesh <diveshpatel39@gmail.com>
2021-05-26 11:00:42 -0700

    made changes to test firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import {webPageBuilder} from './webPageBuilder';
-import firebase from "firebase/app";
 
+const login =(()=>{
+  const authLog = ()=>{
+    const auth = firebase.auth(); 
+    const pageBody = document.body;
+    const loginCenter = document.createElement('div');
+    loginCenter.className='login-style';
+    const signInButton = document.createElement('button');
+    const signOutButton =document.createElement('button');
+    signInButton.className = 'signInButton';
+    signOutButton.className = 'signOutButton';
+    loginCenter.append(signInButton);
+    loginCenter.append(signOutButton);
+    pageBody.appendChild(loginCenter);
+    signInButton.onclick = ()=> auth.signInWithPopup(provider);
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.onAuthStateChanged();
+    webPageBuilder();
 
-webPageBuilder();
+  };
+  return{
+    authLog
+  }
+})();
+
 
 console.log(firebase);
+
+login.authLog();
