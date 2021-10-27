f3f126617ea31e51dd43045725ced61a77541ee4
DPDivesh <diveshpatel39@gmail.com>
2021-10-27 12:55:48 -0700

    created function to get user id

--- src/projectCreate.js
+++ src/projectCreate.js
@@ -14,33 +14,40 @@ return {name,tasks, };
 };
 
   const updateProject = (name,taskName)=>{
+    console.log("update project start")
   let projectName = name;
   let tasks= [];
-  let projectsRef;
-  
-    unsubscribe = tasksRef;
-    projectsRef = db.collection('users').where('uid','==', firebase.auth().currentUser.uid)
-    tasksRef = projectsRef.where('name','==',document.title).orderBy('createdAt')
+  // let projectsRef;
+  // // let tasksRef;
+  let uid;
+  getUserId(uid);
+  console.log(uid,'uid');
+  //   projectsRef =  db.collection('users').where('uid','==', firebase.auth().currentUser.uid)
+    // tasksRef = projectsRef.where('name','==',document.title).orderBy('createdAt')
     // tasksRef =  projectsRef.doc.data().tasks;
-    // console.log(projectsRef);
-    tasksRef.onSnapshot(querySnapshot =>{
-             let task = querySnapshot.docs.map(doc =>{
-               console.log(doc.data().project.tasks.length,'length');
-              //work this out 
-              //maybe send this to a function to build and loop through array
-              let taskHolder =[]
-              for(let i=0; i<doc.data().project.tasks.length;i++){
-              console.log(taskHolder,'taskholder');
-              if(taskHolder.length==doc.data().project.tasks.length){
-                console.log(taskHolder);
-                return taskHolder.join('   ');
-              }
 
-              }
-          
-            });
-            console.log(task)
-          })
+   
+    // projectsRef.get().then((doc) =>{
+    //   console.log(doc[0].data(),'name');
+    //   //https://fireship.io/snippets/read-a-single-firestore-document/
+    //   // console.log(doc.data());
+    //     let task = querySnapshot.docs.map(doc =>{
+    //       console.log(doc.data(),'length');
+    //      //work this out 
+    //      //maybe send this to a function to build and loop through array
+    //      let taskHolder =[]
+    //      for(let i=0; i<doc.data().project.tasks.length;i++){
+    //      console.log(taskHolder,'taskholder');
+    //      if(taskHolder.length==doc.data().project.tasks.length){
+    //        console.log(taskHolder);
+    //        return taskHolder.join('   ');
+    //      }
+
+    //      }
+     
+    //    }); 
+
+    //       })
   tasks.push(taskName);
   // const testFunciton =()=> {
   //   console.log('hi');
@@ -49,9 +56,25 @@ return {name,tasks, };
 return {name,tasks, };
 };
 
+async function getUserId(uid){
+  const projectsRef = await db.collection('users').where('uid','==', firebase.auth().currentUser.uid).get();
+console.log('projkekdkd');
+  if (!projectsRef.empty){
+    const snapshot = projectsRef.docs[0];
+    const data = snapshot.data();
+    console.log(data.uidt);
+    uid = data;
+    return uid;
+  }
+  else{
+    console.log('not found')
+  }
+
+}
+
 export function createTask(taskName,taskRef,projectName,taskDoc){
-  console.log(taskRef)
- let index = taskRef.findIndex(tasks=> tasks===`[${projectName}]`);
+  console.log("update Project")
+//  let index = taskRef.findIndex(tasks=> tasks===`[${projectName}]`);
  
   db.collection('users').doc('sovU3DmGyLVxvInHjJp5N5L7Wkj1'
     ).update("project",firebase.firestore.FieldValue.arrayUnion(updateProject(projectName,taskName)));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
+  mode:"development",
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  
+  plugins: [
+    // fix "process is not defined" error:
+    // (do "npm install process" before running the build)
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  resolve: {
+    fallback: {
+      fs: false
+    }
+  }
 };
